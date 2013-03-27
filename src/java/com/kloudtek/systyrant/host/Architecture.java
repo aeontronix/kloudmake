a92f972f8e3a4267b212195dc61c57726ad199f7
Yannick Menager <ymenager@kloudtek.com>
2013-03-27 19:12:47 +0000

    systyrant-33 Implement @OnlyIfArch

---
+++ src/java/com/kloudtek/systyrant/host/Architecture.java
@@ -0,0 +1,8 @@
+package com.kloudtek.systyrant.host;
+
+/**
+ * Enumeration of CPU architectures
+ */
+public enum Architecture {
+    I386, AMD64, ARM, IA64, ARMEL, MIPS, POWERPC, SPARC, HPPA, M68K, SH
+}
