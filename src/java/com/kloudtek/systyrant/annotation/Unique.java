b103fab636551b2f6bdfc3834b108c8b3a30cecd
Yannick Menager <ymenager@kloudtek.com>
2013-03-29 17:25:17 +0000

    systyrant-35 Restrict uniqueness by fields

--- src/java/com/kloudtek/systyrant/annotation/Unique.java
+++ src/java/com/kloudtek/systyrant/annotation/Unique.java
@@ -9,12 +9,13 @@ import com.kloudtek.systyrant.resource.UniqueScope;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
-@Target({TYPE, METHOD})
+@Target({TYPE, FIELD})
 public @interface Unique {
     UniqueScope value() default UniqueScope.GLOBAL;
 }
